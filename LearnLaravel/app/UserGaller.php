f3f2e9216598f33e12306e432d9a833392651adf
Zohaib Dilawer <zohaibdilawer@live.com>
2018-05-26 20:00:36 +0500

    my first project togit

---
+++ LearnLaravel/app/UserGaller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App;
+
+class UserGaller extends Model
+{
+
+	protected $table='user_gallery';
+}
---
+++ LearnLaravel/resources/views/blog_view.blade.php
@@ -0,0 +1,8 @@
+
+@include('layouts.app')
+    <div class="div">
+        <form>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <button type="submit" btn btn-primary>post</button>
+        </form>
+    </div>
